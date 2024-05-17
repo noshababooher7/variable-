f861e446ed2c37fb01f1f1bc2adce3a29e9c01ad
Noshaba <noshababooher7@gmail.com>
2024-05-17 16:44:23 +0500

    Add files via upload

---
+++ variable.dart
@@ -0,0 +1,38 @@
+void main() {
+  //string
+  String name = "Noshaba";
+  String age = "21";
+  String email = "noshababooher7@gmail.com";
+  String adress = "badin";
+  String rollnumber = "123";
+
+  print(name);
+  print(age);
+  print(email);
+  print(adress);
+  print(rollnumber);
+
+// integer
+
+  int number = 23;
+  int number2 = 43;
+
+  print(number);
+  print(number2);
+
+  //double
+  double hight = 22;
+  double number3 = 32;
+  print(hight);
+  print(number3);
+
+  //Num
+  num age2 = 23;
+  num height2 = 32;
+  print(age2);
+  print(height2);
+  //var
+  var name1 = "Noshaba";
+  var age3 = 21;
+  var height3 = 23.0;
+}
